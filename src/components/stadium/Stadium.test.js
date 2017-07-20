16f621775517654812cf69c3290f73ece6fe25af
Dominic Walker <dom@Dominics-MacBook-Pro.local>
2017-07-20 11:02:03 +0100

    Updated README

--- src/components/stadium/Stadium.test.js
+++ src/components/stadium/Stadium.test.js
@@ -1,7 +1,7 @@
 /* global it, expect, describe, jest */
 
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Stadium from './Stadium';
 
 describe('Stadium component', () => {
@@ -14,15 +14,4 @@ describe('Stadium component', () => {
         const component = shallow(<Stadium />);
         expect(component.find('.stand').length).toEqual(4);
     });
-
-    // describe('Select stand input', () => {
-    //     it('Should call the submitStand function when clicked', () => {
-    //         const submitMock = jest.fn();
-    //         const component = mount(<Stadium submitStand={submitMock} />);
-
-    //         expect(submitMock.mock.calls.length).toEqual(0);
-    //         component.find('.north > input').simulate('click');
-    //         expect(submitMock.mock.calls.length).toEqual(1);
-    //     });
-    // });
 });
