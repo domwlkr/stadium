fbd0204b0ec8ba7d0768014d1966afc1a2b9ac08
Dominic Walker <dom@Dominics-MacBook-Pro.local>
2017-07-21 15:56:26 +0100

    Reformatted the svg into a react component so that it could be imported

--- src/components/stadiumSvg/StadiumSvg.js
+++ src/components/stadiumSvg/StadiumSvg.js
@@ -1,9 +1,10 @@
 import React, { PropTypes } from 'react';
+import SvgStadium from '../../static/stadium';
 
 
 const StadiumSvg = () => (
     <div className="stadium">
-        
+        <SvgStadium />
     </div>
 );
 
